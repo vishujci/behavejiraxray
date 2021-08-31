e00b8682c22cc275fb86bc0594fe080b6cf4422a
vishuhanda <49567970+vishuhanda@users.noreply.github.com>
2021-08-31 21:49:19 +0530

    Update triggerfeaturesfromjira.py

--- triggerfeaturesfromjira.py
+++ triggerfeaturesfromjira.py
@@ -7,7 +7,7 @@ import sys
 
 def getconfig():
     try:
-        print(sys.argv)
+       print(sys.argv)
        config = configparser.ConfigParser()
        config.read(rootpath.detect() + "\\"+sys.argv[1])
     except Exception as e:
