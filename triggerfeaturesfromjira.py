d7328ebfa2c258b4aaa76ac01d56833c64e3ab15
vishu <itsvishuhanda@gmail.com>
2021-08-31 23:32:52 +0530

    added some changes

--- triggerfeaturesfromjira.py
+++ triggerfeaturesfromjira.py
@@ -5,33 +5,36 @@ import configparser
 import rootpath
 import sys
 
+
 def getconfig():
     # try:
     print(sys.argv)
     config = configparser.ConfigParser()
-    config.read(rootpath.detect() + "\\"+sys.argv[1])
+    config.read(rootpath.detect() + "\\" + sys.argv[1])
     # except Exception as e:
-        # raise e
+    # raise e
     return config
 
 
 def get_jira_authentication_token():
     try:
-        body = { "client_id" : "8589A01E21F64F9D96772EF327AF60E2" , "client_secret" : "14a2dcfb46e97cb14e93be613eae25448b02555499d370685ea225ba2d0ff9de"}
-        response  =  requests.post("https://xray.cloud.xpand-it.com/api/v2/authenticate", headers={"Content-Type":"application/json"}, data=json.dumps(body))
+        body = {"client_id": "8589A01E21F64F9D96772EF327AF60E2",
+                "client_secret": "14a2dcfb46e97cb14e93be613eae25448b02555499d370685ea225ba2d0ff9de"}
+        response = requests.post("https://xray.cloud.xpand-it.com/api/v2/authenticate",
+                                 headers={"Content-Type": "application/json"}, data=json.dumps(body))
     except Exception as e:
         raise e
 
-    return response.text.replace('"','')
+    return response.text.replace('"', '')
 
 
-def get_features_files_from_jira(bearer_token: str,ticketkeys: str):
-
+def get_features_files_from_jira(bearer_token: str, ticketkeys: str):
     try:
-        response = requests.get('https://xray.cloud.xpand-it.com/api/v2/export/cucumber?keys=' + ticketkeys,headers={'Content-Type':'application/json','Authorization': 'Bearer ' + bearer_token})
+        response = requests.get('https://xray.cloud.xpand-it.com/api/v2/export/cucumber?keys=' + ticketkeys,
+                                headers={'Content-Type': 'application/json', 'Authorization': 'Bearer ' + bearer_token})
         print(response.text)
         print("printing something")
-        with open("chkfeatures.zip","wb") as code:
+        with open("chkfeatures.zip", "wb") as code:
             code.write(response.content)
         with zipfile.ZipFile("chkfeatures.zip", "r") as zip_ref:
             zip_ref.extractall("features")
@@ -39,18 +42,10 @@ def get_features_files_from_jira(bearer_token: str,ticketkeys: str):
         raise e
 
 
-
-
-
 if __name__ == "__main__":
     config = getconfig()
     sectionsConfig = config.sections()
     sectionsLength = len(sectionsConfig)
     for x in range(sectionsLength):
-        print("hello hello")
         token = get_jira_authentication_token()
-        get_features_files_from_jira(token,config.get(sectionsConfig[x],'tickets'))
-
-
-
-        
+        get_features_files_from_jira(token, config.get(sectionsConfig[x], 'tickets'))
